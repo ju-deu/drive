6db9cd1dbc14eb7d118f1ace3d4a6bf51b46b7d2
ju_deu <fabi_deu@icloud.com>
2025-01-30 21:15:39 +0100

    upload handler

--- migrations/0001_bundled.sql
+++ migrations/0001_bundled.sql
@@ -14,10 +14,15 @@ CREATE TABLE IF NOT EXISTS "users" (
 
 
 
-CREATE TABLE file (
+CREATE TABLE IF NOT EXISTS file (
                       reference_uuid VARCHAR PRIMARY KEY,
                       owner_uuid VARCHAR NOT NULL,
                       filename VARCHAR NOT NULL,
+                      extension VARCHAR NOT NULL,
+
+                      relative_path VARCHAR,
+                      absolute_path VARCHAR,
+                      size BIGINT,
 
                       timestamp bigint DEFAULT EXTRACT(EPOCH FROM NOW())
-)
+);
