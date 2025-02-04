3eda99eaabc3c0e697b19e585327d1baf6b7f6fc
ju_deu <fabi_deu@icloud.com>
2025-02-04 21:16:25 +0100

    might be onto smth idk

--- src/handlers/files/download.rs
+++ src/handlers/files/download.rs
@@ -1,19 +1,22 @@
 use crate::models::appstate::AppstateWrapper;
 use crate::models::file::File;
 use crate::models::user::AuthUser;
-use axum::extract::{Path, State};
-use axum::http::StatusCode;
-use axum::response::{IntoResponse, Response};
+use axum::extract::{Path, Request, State};
+use axum::http::{header, HeaderValue, StatusCode};
 use axum::Extension;
 use std::path::Path as StdPath;
-use tower_http::services::ServeDir;
+use axum::response::IntoResponse;
+use tower::ServiceExt;
+use tower_http::services::ServeFile;
 use uuid::Uuid;
 
+#[axum_macros::debug_handler]
 pub async fn serve_file(
     State(appstate): State<AppstateWrapper>,
     auth_user: Extension<AuthUser>,
     Path(ref_id): Path<Uuid>,
-) -> Result<Response, (StatusCode, &'static str)> {
+    req: Request,
+) -> Result<axum_core::response::Response, (StatusCode, &'static str)> {
     let user = auth_user.0.0;
     let appstate = appstate.0;
 
@@ -29,13 +32,18 @@ pub async fn serve_file(
         return Err((StatusCode::NOT_FOUND, "Failed to find file on disk"))
     }
 
-    // construct real filename to not serve filed named after uuids
-    let actual_filename = format!("{}/{}/{}", &appstate.file_location, &user.uuid.to_string(), file.filename);
+    // construct ServeFile and response
+    let service = ServeFile::new(&file.absolute_path);
 
-    // serve file with respective filename
-    let service = ServeDir::new(file.absolute_path);
-    match service.try_call(actual_filename).await {
-        Ok(o) => Ok(o.into_response()),
-        Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to serve file"))
+    let mut response = service.oneshot(req).await.map_err(|_| (StatusCode::NOT_FOUND, "Failed to construct file service"))?;
+
+    // set custom headers for original filename
+    let header_value = format!("attachment; filename=\"{}\"", &file.filename);
+    if let Ok(value) = HeaderValue::from_str(&header_value) {
+        response.headers_mut().insert(header::CONTENT_DISPOSITION, value);
+    } else {
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to construct headers"))
     }
+
+    Ok(response.into_response())
 }
